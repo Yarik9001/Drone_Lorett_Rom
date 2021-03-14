e6951433f3226afd6aedee374611d7fe9b5233e5
Yarik9001 <eyenot2@yandex.ru>
2021-03-14 23:18:53 +0300

    test_one

---
+++ traslite.py
@@ -0,0 +1,14 @@
+def trans_trek(old_name: str, new_name: str):
+    '''
+    old_neme - original satellite track file name
+    new_name - final filename of the satellite track
+    '''
+    try:
+        with open("test.txt") as file_handler:
+            print(1)
+    except:
+        print(0)
+
+
+
+trans_trek()
