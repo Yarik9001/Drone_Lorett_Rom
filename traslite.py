0b772b21ec9d73400b7c764d08ecc166f79b99bc
Yarik9001 <eyenot2@yandex.ru>
2021-03-15 07:22:42 +0300

    translete

--- traslite.py
+++ traslite.py
@@ -1,15 +1,28 @@
-def trans_trek(old_name: str, new_name: str):
+from math import sin, cos, radians
+
+
+def trans_trek(old_name: str, new_name: str, deb=False):
     '''
     old_neme - original satellite track file name
     new_name - final filename of the satellite track
     '''
     try:
-        with open("test.txt") as file_handler:
-            print(1)
+        with open(old_name) as file:
+            file = file.read().split('\n')
+            file_new = open(new_name, 'w')
+            for i in file[:6]:
+                file_new.write(f'{i}\n')
+            for i in file[6:-1]:
+                mass = i.split()
+                for a in range(len(mass)):
+                    mass[a] = '.'.join(mass[a].split(':'))
+                x = round(1 + sin(radians(float(mass[1]))), 4)
+                y = round(1 + cos(radians(float(mass[2]))), 4)
+                time = mass[0]
+                file_new.write(f'{time}   {x}   {y}\n')
+            file_new.close()
     except:
-        print(0)
-# тестовый 
+        print('Satellite track translation error')
 
 
-
-trans_trek()
+trans_trek('test_trek_old.txt.txt', 'new.txt')
