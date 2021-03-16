d1c937ee40aaf0bd1f84daaa689b288ecc624bcd
Yarik9001 <eyenot2@yandex.ru>
2021-03-16 07:03:48 +0300

    bekup

--- traslite.py
+++ traslite.py
@@ -1,4 +1,4 @@
-from math import sin, cos, radians
+import matplotlib.pyplot as plt
 
 
 def trans_trek(old_name: str, new_name: str, deb=False):
@@ -6,23 +6,49 @@ def trans_trek(old_name: str, new_name: str, deb=False):
     old_neme - original satellite track file name
     new_name - final filename of the satellite track
     '''
+    from math import sin, cos, radians
     try:
-        with open(old_name) as file:
-            file = file.read().split('\n')
-            file_new = open(new_name, 'w')
-            for i in file[:6]:
+        with open(old_name) as file:  # open old
+            file = file.read().split('\n')  # reading
+            file_new = open(new_name, 'w')  # creating a new file
+            for i in file[:3]:  # system information recording
                 file_new.write(f'{i}\n')
-            for i in file[6:-1]:
+            file_new.write('Time (UTC)  X (metr)  Y (metr)\n\n')
+            for i in file[6:-1]:  # translation and recording of coordinates
                 mass = i.split()
                 for a in range(len(mass)):
                     mass[a] = '.'.join(mass[a].split(':'))
-                x = round(1 + sin(radians(float(mass[1]))), 4)
-                y = round(1 + cos(radians(float(mass[2]))), 4)
+                azimuth = radians(float(mass[1]))
+                elevation = 
+                h1 = 
+                x = round(1 + sin(radians(float(mass[1]))), 5)  # translation
+                y = round(1 + cos(radians(float(mass[2]))), 5)
                 time = mass[0]
-                file_new.write(f'{time}   {x}   {y}\n')
+                file_new.write(f'{time}\t{x}\t{y}\n')  # write new data
             file_new.close()
     except:
         print('Satellite track translation error')
 
-
 trans_trek('test_trek_old.txt.txt', 'new.txt')
+
+
+def track_show(name: str):
+    ''' Function for visualizing the satellite track '''
+    x, y = [], []
+    with open(name) as file:
+        file = file.read().split('\n')[6:-1]
+        for i in file:
+            print(i)
+            i = i.split('\t')
+            x.append(float(i[1]))
+            y.append(float(i[2]))
+        print(x)
+        print(y)
+        plt.title('Track show')
+        plt.xlabel("x")
+        plt.ylabel("y")
+        plt.plot(y, x)
+        plt.show()
+
+
+track_show('new.txt')
